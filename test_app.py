0d0db08d117db040936845e40976ad33f22f320b
vvithurshan <vvithurshan@gmail.com>
2026-07-08 02:17:36 -0600

    bug fixed test_app

--- test_app.py
+++ test_app.py
@@ -2,7 +2,8 @@ from app import app
 
 # Need to start with test
 def test_home():
-    response = app.test_client("/")
+    client = app.test_client()
+    response = client.get("/")
 
     assert response.status_code == 200
     assert response.data == b"<h1>Hello World</h1>"
